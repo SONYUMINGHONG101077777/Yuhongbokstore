8a428d43675dad8672467407d8b9dcd518f200ab
SONYUMINGHONG101077777 <164879589+SONYUMINGHONG101077777@users.noreply.github.com>
2024-10-27 11:24:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function addToCart(bookTitle, price) {
+    alert(`${bookTitle} has been added to your cart at $${price}.`);
+    // You can expand this function to actually manage a shopping cart
+}
